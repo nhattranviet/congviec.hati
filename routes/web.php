17e0ad72b160c43daccb7ca2b2cf32812bba9a3d
nhatcaht <nhatcaht@gmail.com>
2018-07-29 09:46:16 +0700

    Bat dau lam phan quyen

--- routes/web.php
+++ routes/web.php
@@ -30,73 +30,73 @@ Route::get('/logout', 'NguoidungController@getLogout')->name('getLogout');
 Route::post('/change-password/', 'NguoidungController@changePassword')->name('nguoi-dung-changepassword');
 //------------------------------END NGƯỜI DÙNG ---------------------------
 Route::get('/tai-khoan', 'TaiKhoanController@index');
-Route::resource('/nhan-khau', 'NhanKhauController');
-Route::post('/nhan-khau/store', 'NhanKhauController@store')->name('nhankhau.store');
-Route::get('/nhan-khau/ajax_index', 'NhanKhauController@index')->name('nhankhau.ajax_index');
-Route::post('/nhan-khau/{id}', 'NhanKhauController@update')->name('nhankhau.update');
-Route::get('/nhan-khau/provinces/{id}', 'NhanKhauController@getProvinces');
-Route::get('/nhan-khau/districts/{id}', 'NhanKhauController@getDistricts');
-Route::get('/nhan-khau/wards/{id}', 'NhanKhauController@getWards');
+Route::resource('/nhan-khau', 'Nhanhokhau\NhanKhauController');
+Route::post('/nhan-khau/store', 'Nhanhokhau\NhanKhauController@store')->name('nhankhau.store');
+Route::get('/nhan-khau/ajax_index', 'Nhanhokhau\NhanKhauController@index')->name('nhankhau.ajax_index');
+Route::post('/nhan-khau/{id}', 'Nhanhokhau\NhanKhauController@update')->name('nhankhau.update');
+Route::get('/nhan-khau/provinces/{id}', 'Nhanhokhau\NhanKhauController@getProvinces');
+Route::get('/nhan-khau/districts/{id}', 'Nhanhokhau\NhanKhauController@getDistricts');
+Route::get('/nhan-khau/wards/{id}', 'Nhanhokhau\NhanKhauController@getWards');
 
-Route::get('/nhan-khau/{id}/dang-ky-thuong-tru', 'NhanKhauController@getDangkythuongtru');
-Route::post('/nhan-khau/{id}/dang-ky-thuong-tru', 'NhanKhauController@postDangkythuongtru')->name('dang-ky-thuong-tru');
+Route::get('/nhan-khau/{id}/dang-ky-thuong-tru', 'Nhanhokhau\NhanKhauController@getDangkythuongtru');
+Route::post('/nhan-khau/{id}/dang-ky-thuong-tru', 'Nhanhokhau\NhanKhauController@postDangkythuongtru')->name('dang-ky-thuong-tru');
 
-Route::get('/nhan-khau/{idhoso}/chi-tiet-ho-khau', 'NhanKhauController@getChitiethokhau')->name('chi-tiet-ho-khau');
-Route::get('/nhan-khau/{idnhankhau}/chi-tiet-nhan-khau', 'NhanKhauController@getChitietnhankhau');
+Route::get('/nhan-khau/{idhoso}/chi-tiet-ho-khau', 'Nhanhokhau\NhanKhauController@getChitiethokhau')->name('chi-tiet-ho-khau');
+Route::get('/nhan-khau/{idnhankhau}/chi-tiet-nhan-khau', 'Nhanhokhau\NhanKhauController@getChitietnhankhau');
 
-Route::get('/nhan-khau/{id}/sua-nhan-khau', 'NhanKhauController@getSuanhankhau');
-Route::post('/nhan-khau/{id}/sua-nhan-khau', 'NhanKhauController@postSuanhankhau')->name('sua-nhan-khau');
+Route::get('/nhan-khau/{id}/sua-nhan-khau', 'Nhanhokhau\NhanKhauController@getSuanhankhau');
+Route::post('/nhan-khau/{id}/sua-nhan-khau', 'Nhanhokhau\NhanKhauController@postSuanhankhau')->name('sua-nhan-khau');
 
-Route::get('/nhan-khau/{id}/check-xoa-thuong-tru', 'NhanKhauController@getCheckxoathuongtru');
-Route::post('/nhan-khau/{id}/xoa-thuong-tru', 'NhanKhauController@xoaThuongtru')->name('xoa-thuong-tru');
+Route::get('/nhan-khau/{id}/check-xoa-thuong-tru', 'Nhanhokhau\NhanKhauController@getCheckxoathuongtru');
+Route::post('/nhan-khau/{id}/xoa-thuong-tru', 'Nhanhokhau\NhanKhauController@xoaThuongtru')->name('xoa-thuong-tru');
 
-Route::get('/nhan-khau/{idhoso}/check-xoa-thuong-tru-HDG', 'NhanKhauController@getCheckxoathuongtruHGD');
-Route::post('/nhan-khau/{idhoso}/xoa-thuong-tru-HGD', 'NhanKhauController@xoaThuongtruHGD')->name('xoa-thuong-tru-HDG');
+Route::get('/nhan-khau/{idhoso}/check-xoa-thuong-tru-HDG', 'Nhanhokhau\NhanKhauController@getCheckxoathuongtruHGD');
+Route::post('/nhan-khau/{idhoso}/xoa-thuong-tru-HGD', 'Nhanhokhau\NhanKhauController@xoaThuongtruHGD')->name('xoa-thuong-tru-HDG');
 
-Route::get('/nhan-khau/{idhoso}/check-cap-lai-SHK', 'NhanKhauController@getCheckcaplaiSHK')->name('get-check-cap-lai-SHK');
-Route::post('/nhan-khau/{idhoso}/cap-lai-SHK', 'NhanKhauController@caplaiSHK')->name('post-cap-lai-SHK');
+Route::get('/nhan-khau/{idhoso}/check-cap-lai-SHK', 'Nhanhokhau\NhanKhauController@getCheckcaplaiSHK')->name('get-check-cap-lai-SHK');
+Route::post('/nhan-khau/{idhoso}/cap-lai-SHK', 'Nhanhokhau\NhanKhauController@caplaiSHK')->name('post-cap-lai-SHK');
 
-Route::get('/nhan-khau/{idhoso}/check-cap-doi-SHK', 'NhanKhauController@getCheckcapdoiSHK')->name('get-check-cap-doi-SHK');
-Route::post('/nhan-khau/{idhoso}/cap-doi-SHK', 'NhanKhauController@capdoiSHK')->name('post-cap-doi-SHK');
+Route::get('/nhan-khau/{idhoso}/check-cap-doi-SHK', 'Nhanhokhau\NhanKhauController@getCheckcapdoiSHK')->name('get-check-cap-doi-SHK');
+Route::post('/nhan-khau/{idhoso}/cap-doi-SHK', 'Nhanhokhau\NhanKhauController@capdoiSHK')->name('post-cap-doi-SHK');
 
-Route::get('/nhan-khau/{idhoso}/thay-doi-chu-ho', 'NhanKhauController@getThaydoichuho');
-Route::post('/nhan-khau/{idhoso}/thay-doi-chu-ho', 'NhanKhauController@postThaydoichuho')->name('thay-doi-chu-ho');
+Route::get('/nhan-khau/{idhoso}/thay-doi-chu-ho', 'Nhanhokhau\NhanKhauController@getThaydoichuho');
+Route::post('/nhan-khau/{idhoso}/thay-doi-chu-ho', 'Nhanhokhau\NhanKhauController@postThaydoichuho')->name('thay-doi-chu-ho');
 
-Route::get('/nhan-khau/{idhoso}/tach-ho-khau', 'NhanKhauController@getTachhokhau')->name('get-tach-ho-khau');
-Route::post('/nhan-khau/{idhoso}/tach-ho-khau', 'NhanKhauController@postTachhokhau')->name('post-tach-ho-khau');
+Route::get('/nhan-khau/{idhoso}/tach-ho-khau', 'Nhanhokhau\NhanKhauController@getTachhokhau')->name('get-tach-ho-khau');
+Route::post('/nhan-khau/{idhoso}/tach-ho-khau', 'Nhanhokhau\NhanKhauController@postTachhokhau')->name('post-tach-ho-khau');
 
-Route::get('/nhan-khau/{idhoso}/get-ho-khau-nhap', 'NhanKhauController@getNhaphokhau')->name('get-nhap-ho-khau');
-Route::get('/nhan-khau/{idhosogoc}/{idhosonhap}/nhap-ho-khau', 'NhanKhauController@getHoNhap')->name('get-ho-khau-de-nhap');
-Route::get('/search-ho-so', 'NhanKhauController@postTimkiemhoso')->name('post-tim-kiem-ho-so');
-Route::post('/nhan-khau/{idhosogoc}/{idhosonhap}/nhap-ho-khau', 'NhanKhauController@postNhaphokhau')->name('post-nhap-ho-khau');
+Route::get('/nhan-khau/{idhoso}/get-ho-khau-nhap', 'Nhanhokhau\NhanKhauController@getNhaphokhau')->name('get-nhap-ho-khau');
+Route::get('/nhan-khau/{idhosogoc}/{idhosonhap}/nhap-ho-khau', 'Nhanhokhau\NhanKhauController@getHoNhap')->name('get-ho-khau-de-nhap');
+Route::get('/search-ho-so', 'Nhanhokhau\NhanKhauController@postTimkiemhoso')->name('post-tim-kiem-ho-so');
+Route::post('/nhan-khau/{idhosogoc}/{idhosonhap}/nhap-ho-khau', 'Nhanhokhau\NhanKhauController@postNhaphokhau')->name('post-nhap-ho-khau');
 
-Route::get('/bao-cao-nhan-khau', 'BaocaoThongkeController@getBaocaonhankhau')->name('get-bao-cao-nhan-khau');
-// Route::get('/post-bao-cao-nhan-khau', 'NhanKhauController@getBaocaonhankhauToResult')->name('post-bao-cao-nhan-khau');
-Route::get('/get-bao-cao-nhan-khau', 'BaocaoThongkeController@getBaocaonhankhauToResult')->name('post-bao-cao-nhan-khau');
+Route::get('/bao-cao-nhan-khau', 'Nhanhokhau\BaocaoThongkeController@getBaocaonhankhau')->name('get-bao-cao-nhan-khau');
+// Route::get('/post-bao-cao-nhan-khau', 'Nhanhokhau\NhanKhauController@getBaocaonhankhauToResult')->name('post-bao-cao-nhan-khau');
+Route::get('/get-bao-cao-nhan-khau', 'Nhanhokhau\BaocaoThongkeController@getBaocaonhankhauToResult')->name('post-bao-cao-nhan-khau');
 
-// Route::get('/thong-ke/{ngaydau?}/{ngaycuoi?}', 'NhanKhauController@getThongke')->name('thong-ke');
-Route::get('/thong-ke', 'BaocaoThongkeController@getThongke')->name('thong-ke');
-Route::get('/get-thong-ke-nhan-khau', 'BaocaoThongkeController@getThongkeToResult')->name('get-thong-ke-nhan-khau');
+// Route::get('/thong-ke/{ngaydau?}/{ngaycuoi?}', 'Nhanhokhau\NhanKhauController@getThongke')->name('thong-ke');
+Route::get('/thong-ke', 'Nhanhokhau\BaocaoThongkeController@getThongke')->name('thong-ke');
+Route::get('/get-thong-ke-nhan-khau', 'Nhanhokhau\BaocaoThongkeController@getThongkeToResult')->name('get-thong-ke-nhan-khau');
 
 //-------------------TẠM TRÚ--------------------------
-Route::resource('/tam-tru', 'TamtruController');
-Route::get('/tam-tru/{idsotamtru}/chi-tiet-so-tam-tru', 'TamtruController@getChitietSotamtru')->name('chi-tiet-so-tam-tru');
-Route::get('/tam-tru/{idnhankhau}/{idsotamtru}/chi-tiet-nhan-khau', 'TamtruController@getChitietnhankhauTamtru')->name('chi-tiet-nhan-khau-tam-tru');
+Route::resource('/tam-tru', 'Nhanhokhau\TamtruController');
+Route::get('/tam-tru/{idsotamtru}/chi-tiet-so-tam-tru', 'Nhanhokhau\TamtruController@getChitietSotamtru')->name('chi-tiet-so-tam-tru');
+Route::get('/tam-tru/{idnhankhau}/{idsotamtru}/chi-tiet-nhan-khau', 'Nhanhokhau\TamtruController@getChitietnhankhauTamtru')->name('chi-tiet-nhan-khau-tam-tru');
 
-Route::get('/tam-tru/{idnhankhau}/{idsotamtru}/sua-nhan-khau', 'TamtruController@getSuanhankhau')->name('get-sua-nhan-khau-tam-tru');
-Route::post('/tam-tru/{idnhankhau}/{idsotamtru}/sua-nhan-khau', 'TamtruController@postSuanhankhau')->name('post-sua-nhan-khau-tam-tru');
+Route::get('/tam-tru/{idnhankhau}/{idsotamtru}/sua-nhan-khau', 'Nhanhokhau\TamtruController@getSuanhankhau')->name('get-sua-nhan-khau-tam-tru');
+Route::post('/tam-tru/{idnhankhau}/{idsotamtru}/sua-nhan-khau', 'Nhanhokhau\TamtruController@postSuanhankhau')->name('post-sua-nhan-khau-tam-tru');
 
-Route::get('/tam-tru/{idsotamtru}/get-add-nhan-khau', 'TamtruController@getAddnhankhau')->name('get-add-nhan-khau-tam-tru');
-Route::post('/tam-tru/{idsotamtru}/post-add-nhan-khau', 'TamtruController@postAddnhankhau')->name('post-add-nhan-khau-tam-tru');
+Route::get('/tam-tru/{idsotamtru}/get-add-nhan-khau', 'Nhanhokhau\TamtruController@getAddnhankhau')->name('get-add-nhan-khau-tam-tru');
+Route::post('/tam-tru/{idsotamtru}/post-add-nhan-khau', 'Nhanhokhau\TamtruController@postAddnhankhau')->name('post-add-nhan-khau-tam-tru');
 
-Route::get('/get-add-so-tam-tru-ca-nhan', 'TamtruController@getAddSoTamTruCaNhan')->name('get-add-so-tam-tru-ca-nhan');
-Route::post('/post-add-so-tam-tru-ca-nhan', 'TamtruController@postAddSoTamTruCaNhan')->name('post-add-so-tam-tru-ca-nhan');
+Route::get('/get-add-so-tam-tru-ca-nhan', 'Nhanhokhau\TamtruController@getAddSoTamTruCaNhan')->name('get-add-so-tam-tru-ca-nhan');
+Route::post('/post-add-so-tam-tru-ca-nhan', 'Nhanhokhau\TamtruController@postAddSoTamTruCaNhan')->name('post-add-so-tam-tru-ca-nhan');
 
-Route::get('/tam-tru/{idnhankhau}/{idsotamtru}/get-xoa-tam-tru-nhan-khau', 'TamtruController@getXoaTamTruNhanKhau')->name('get-xoa-tam-tru-nhan-khau');
-Route::post('/tam-tru/{idnhankhau}/{idsotamtru}/post-xoa-tam-tru-nhan-khau', 'TamtruController@postXoaTamTruNhanKhau')->name('post-xoa-tam-tru-nhan-khau');
+Route::get('/tam-tru/{idnhankhau}/{idsotamtru}/get-xoa-tam-tru-nhan-khau', 'Nhanhokhau\TamtruController@getXoaTamTruNhanKhau')->name('get-xoa-tam-tru-nhan-khau');
+Route::post('/tam-tru/{idnhankhau}/{idsotamtru}/post-xoa-tam-tru-nhan-khau', 'Nhanhokhau\TamtruController@postXoaTamTruNhanKhau')->name('post-xoa-tam-tru-nhan-khau');
 
-Route::get('/tam-tru/{idnhankhau}/{idsotamtru}/get-gia-han-tam-tru-nhan-khau', 'TamtruController@getGiaHanTamTruNhanKhau')->name('get-gia-han-tam-tru-nhan-khau');
-Route::post('/tam-tru/{idnhankhau}/{idsotamtru}/post-gia-han-tam-tru-nhan-khau', 'TamtruController@postGiaHanTamTruNhanKhau')->name('post-gia-han-tam-tru-nhan-khau');
+Route::get('/tam-tru/{idnhankhau}/{idsotamtru}/get-gia-han-tam-tru-nhan-khau', 'Nhanhokhau\TamtruController@getGiaHanTamTruNhanKhau')->name('get-gia-han-tam-tru-nhan-khau');
+Route::post('/tam-tru/{idnhankhau}/{idsotamtru}/post-gia-han-tam-tru-nhan-khau', 'Nhanhokhau\TamtruController@postGiaHanTamTruNhanKhau')->name('post-gia-han-tam-tru-nhan-khau');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
